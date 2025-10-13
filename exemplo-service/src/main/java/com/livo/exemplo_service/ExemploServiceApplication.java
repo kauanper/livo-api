b5cedc713f489c55d9e6847a1ab7160b209c4882
kauan <kauancavalcante8p@gmail.com>
2025-10-13 17:11:25 -0300

    add educational microserver for team alignment

---
+++ exemplo-service/src/main/java/com/livo/exemplo_service/ExemploServiceApplication.java
@@ -0,0 +1,13 @@
+package com.livo.exemplo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploServiceApplication.class, args);
+	}
+
+}
---
+++ exemplo-service/src/test/java/com/livo/exemplo_service/ExemploServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livo.exemplo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
