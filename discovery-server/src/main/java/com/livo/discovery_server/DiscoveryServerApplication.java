a380c38f547a55d02ab1e4567071d00ffb71ac8a
kauan <kauancavalcante8p@gmail.com>
2025-10-14 23:39:41 -0300

    add: configs

--- discovery-server/src/main/java/com/livo/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/livo/discovery_server/DiscoveryServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
