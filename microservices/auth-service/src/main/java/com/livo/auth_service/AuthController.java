38be0a35d93a463fa0fc2e375131c0b689b369d8
kauan <kauancavalcante8p@gmail.com>
2025-10-23 17:13:02 -0300

    teste: validando controller

--- microservices/auth-service/src/main/java/com/livo/auth_service/AuthController.java
+++ microservices/auth-service/src/main/java/com/livo/auth_service/AuthController.java
@@ -1,11 +1,15 @@
 package com.livo.auth_service;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
-    @GetMapping("/auth/test")
+
+
+    @GetMapping("/test")
     public String testEndpoint() {
         System.out.println("Endpoint de teste chamado!");
         return "Auth-Service funcionando!";
