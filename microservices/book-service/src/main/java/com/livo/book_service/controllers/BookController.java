4843c609baeff206cb1025fc3c6196f1b8bb2c80
kauan <kauancavalcante8p@gmail.com>
2025-11-06 00:57:26 -0300

    add: controller de getBookById

--- microservices/book-service/src/main/java/com/livo/book_service/controllers/BookController.java
+++ microservices/book-service/src/main/java/com/livo/book_service/controllers/BookController.java
@@ -1,6 +1,7 @@
 package com.livo.book_service.controllers;
 
 import com.livo.book_service.dtos.BookSummaryResponse;
+import com.livo.book_service.services.GetBookByIdUseCase;
 import com.livo.book_service.services.SearchBooksCombinedUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +16,20 @@ public class BookController {
     @Autowired
     private SearchBooksCombinedUseCase searchBooksCombinedUseCase;
 
+    @Autowired
+    GetBookByIdUseCase getBookByIdUseCase;
+
     @GetMapping("/search")
     public ResponseEntity<List<BookSummaryResponse>> searchBooks(
             @RequestParam String query) {
-
         List<BookSummaryResponse> response = searchBooksCombinedUseCase.execute(query);
-
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookSummaryResponse> getBookById(@PathVariable String id) {
+        BookSummaryResponse book = getBookByIdUseCase.execute(id);
+        return ResponseEntity.ok(book);
     }
 
 }
