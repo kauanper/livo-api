41d16381de4314d50d0046f19206ab37822596a8
kauan <kauancavalcante8p@gmail.com>
2025-11-06 23:30:27 -0300

    add: implementação de ExternalApiException na GoogleBooksClient

--- microservices/book-service/src/main/java/com/livo/book_service/APIs/GoogleBooksClient.java
+++ microservices/book-service/src/main/java/com/livo/book_service/APIs/GoogleBooksClient.java
@@ -1,8 +1,11 @@
 package com.livo.book_service.APIs;
 
 import com.livo.book_service.dtos.BookResponse;
+import com.livo.book_service.exceptions.custom.ExternalApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -15,21 +18,37 @@ public class GoogleBooksClient {
     private final RestTemplate restTemplate;
 
     public BookResponse searchBooks(String query) {
-        URI uri = UriComponentsBuilder
-                .fromHttpUrl("https://www.googleapis.com/books/v1/volumes")
-                .queryParam("q", query)
-                .build()
-                .encode()
-                .toUri();
-        return restTemplate.getForObject(uri, BookResponse.class);
+        try {
+            URI uri = UriComponentsBuilder
+                    .fromHttpUrl("https://www.googleapis.com/books/v1/volumes")
+                    .queryParam("q", query)
+                    .build()
+                    .encode()
+                    .toUri();
+
+            return restTemplate.getForObject(uri, BookResponse.class);
+
+        } catch (RestClientException ex) {
+            throw new ExternalApiException("Erro ao consultar Google Books: " + ex.getMessage());
+        }
     }
 
     public BookResponse.BookItem getBookById(String volumeId) {
-        URI uri = UriComponentsBuilder
-                .fromHttpUrl("https://www.googleapis.com/books/v1/volumes/" + volumeId)
-                .build()
-                .encode()
-                .toUri();
-        return restTemplate.getForObject(uri, BookResponse.BookItem.class);
+        try {
+            URI uri = UriComponentsBuilder
+                    .fromHttpUrl("https://www.googleapis.com/books/v1/volumes/" + volumeId)
+                    .build()
+                    .encode()
+                    .toUri();
+
+            return restTemplate.getForObject(uri, BookResponse.BookItem.class);
+
+        } catch (HttpClientErrorException.NotFound ex) {
+            //responsabilidade da camada service
+            return null;
+
+        } catch (RestClientException ex) {
+            throw new ExternalApiException("Erro ao consultar Google Books: " + ex.getMessage());
+        }
     }
 }
