e1c778220eb9f6740f406c774ef8742b01021bbe
kauan <kauancavalcante8p@gmail.com>
2025-11-11 01:57:20 -0300

    add: refatorando SearchBooksUseCase

--- microservices/book-service/src/main/java/com/livo/book_service/services/search/SearchBooksUseCase.java
+++ microservices/book-service/src/main/java/com/livo/book_service/services/search/SearchBooksUseCase.java
@@ -1,4 +1,55 @@
 package com.livo.book_service.services.search;
 
+import com.livo.book_service.dtos.BookResponse;
+import com.livo.book_service.dtos.BookSummaryResponse;
+import com.livo.book_service.exceptions.custom.EmptyBookListException;
+import com.livo.book_service.exceptions.custom.InvalidRequestException;
+import com.livo.book_service.mappers.BookMapper;
+import com.livo.book_service.services.search.order_strategies.OrderByStrategy;
+import com.livo.book_service.services.search.order_strategies.OrderByStrategyFactory;
+import com.livo.book_service.services.search.q_strategies.SearchStrategy;
+import com.livo.book_service.services.search.q_strategies.SearchStrategyFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SearchBooksUseCase {
+
+    private final SearchStrategyFactory searchStrategyFactory;
+    private final OrderByStrategyFactory orderByStrategyFactory;
+    private final BookMapper mapper;
+
+    public List<BookSummaryResponse> execute(String query, String type, String orderBy) {
+
+        validate(query, type, orderBy);
+
+        // Strategy para tipo de busca (q)
+        SearchStrategy searchStrategy = searchStrategyFactory.get(type);
+        List<BookResponse.BookItem> items = searchStrategy.search(query, orderBy);
+
+        if (items.isEmpty()) {
+            throw new EmptyBookListException("Nenhum livro encontrado.");
+        }
+
+        // Strategy para ordenação
+        OrderByStrategy orderStrategy = orderByStrategyFactory.get(orderBy);
+        items = orderStrategy.sort(items);
+
+        return items.stream()
+                .map(mapper::toSummary)
+                .toList();
+    }
+
+    private void validate(String query, String type, String orderBy) {
+        if (query == null || query.isBlank())
+            throw new InvalidRequestException("Query inválida.");
+        if (type == null || type.isBlank())
+            throw new InvalidRequestException("Tipo de busca obrigatório.");
+        if (orderBy == null || orderBy.isBlank())
+            throw new InvalidRequestException("OrderBy obrigatório.");
+    }
 }
+
