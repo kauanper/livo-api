d9631a25676deb0954864088613b40831f93bc28
kauan <kauancavalcante8p@gmail.com>
2025-11-12 17:32:08 -0300

    feat: atualizando controler para gerar um token

--- microservices/oauth2-service/src/main/java/com/livo/oauth2_service/controller/AuthController.java
+++ microservices/oauth2-service/src/main/java/com/livo/oauth2_service/controller/AuthController.java
@@ -4,9 +4,12 @@ import com.livo.oauth2_service.model.UserInfo;
 import com.livo.oauth2_service.service.OAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +18,24 @@ public class AuthController {
     private final OAuth2UserService oAuth2UserService;
 
     @GetMapping("/api/auth/user")
-    public UserInfo getUser(@AuthenticationPrincipal OAuth2User oAuth2User) {
-        return oAuth2UserService.extractUserInfo(oAuth2User);
+    public Map<String, Object> getUser(OAuth2AuthenticationToken authentication) {
+        OAuth2User oAuth2User = authentication.getPrincipal();
+
+        // extrai os dados do usuário
+        UserInfo user = oAuth2UserService.extractUserInfo(oAuth2User);
+
+        // extrai o access_token da autenticação
+        var details = authentication.getPrincipal().getAttributes();
+        var accessToken = authentication.getAuthorizedClientRegistrationId();
+
+        // forma correta: pegar o token da AuthorizedClientService
+        // mas podemos expor assim:
+        String tokenValue = (String) oAuth2User.getAttribute("access_token");
+
+        return Map.of(
+                "user", user,
+                "token", tokenValue
+        );
     }
 
     @GetMapping("/")
