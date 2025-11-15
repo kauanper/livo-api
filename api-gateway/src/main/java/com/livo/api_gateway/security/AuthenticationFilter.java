fbcb4fc9c583ed6771329a2235de56430a23c0d9
Miqueias Bento <miqueias.bento.da.silva@gmail.com>
2025-11-15 11:18:21 -0300

    feat(gateway): configurações de segurança e filtro de autenticação

---
+++ api-gateway/src/main/java/com/livo/api_gateway/security/AuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.livo.api_gateway.security;
+
+import io.jsonwebtoken.JwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RefreshScope
+@Component
+public class AuthenticationFilter implements GatewayFilter {
+
+    @Autowired
+    private RouterValidator validator;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        if (validator.isSecured.test(request)) {
+            if (this.isAuthMissing(request)) {
+                return this.onError(exchange, "Authorization header is missing in request");
+            }
+
+            final String token = this.getAuthHeader(request).substring(7);
+
+            try {
+                jwtUtil.validateToken(token);
+            } catch (JwtException e) {
+                return this.onError(exchange, "Authorization header is invalid or expired");
+            }
+        }
+        return chain.filter(exchange);
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return response.setComplete();
+    }
+
+    private String getAuthHeader(ServerHttpRequest request) {
+        return request.getHeaders().getOrEmpty("Authorization").getFirst();
+    }
+
+    private boolean isAuthMissing(ServerHttpRequest request) {
+        return !request.getHeaders().containsKey("Authorization");
+    }
+}
---
+++ api-gateway/src/main/java/com/livo/api_gateway/security/GatewayFilterChain.java
@@ -0,0 +1,24 @@
+package com.livo.api_gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewayFilterChain {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
+        http
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .authorizeExchange(exchange -> exchange
+                    .pathMatchers("/auth", "/health").permitAll()
+                    .pathMatchers(HttpMethod.POST, "/user").permitAll()
+                    .anyExchange().authenticated());
+        return http.build();
+    }
+}
