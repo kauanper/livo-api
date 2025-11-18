9195cd43e9bb18c59305ada7578d2965979da958
kauan <kauancavalcante8p@gmail.com>
2025-11-18 20:01:09 -0300

    add: association registerDTO

---
+++ microservices/library-service/src/main/java/com/livo/library_service/library/dtos/AssociationRegisterDTO.java
@@ -0,0 +1,21 @@
+package com.livo.library_service.library.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssociationRegisterDTO {
+    @NotNull(message = "UserId é obrigatório")
+    private UUID userId;
+
+    @NotNull(message = "BookId é obrigatório")
+    private String bookId;
+
+    private String thumbnail;
+}
