674081c73f6e9d74edbeb0298182ff768554b901
kauan <kauancavalcante8p@gmail.com>
2025-11-18 20:18:24 -0300

    add: dto de response para association

---
+++ microservices/library-service/src/main/java/com/livo/library_service/library/dtos/AssociationResponseDTO.java
@@ -0,0 +1,9 @@
+package com.livo.library_service.library.dtos;
+
+public record AssociationResponseDTO(
+        Long id,
+        String thumbnail,
+        Integer readingProgress,
+        Integer personalRatting
+) {
+}
---
+++ microservices/library-service/src/main/java/com/livo/library_service/library/services/CreateAssociationUseCase.java
@@ -0,0 +1,8 @@
+package com.livo.library_service.library.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateAssociationUseCase {
+
+}
