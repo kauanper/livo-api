86ad7207bce560bc9a78a7e03fb954288dd87da0
kauan <kauancavalcante8p@gmail.com>
2025-11-19 16:45:15 -0300

    add: Http status em ApplicationException para ficar mais personalizada

--- microservices/library-service/src/main/java/com/livo/library_service/shared/globalExceptions/ApplicationException.java
+++ microservices/library-service/src/main/java/com/livo/library_service/shared/globalExceptions/ApplicationException.java
@@ -1,14 +1,23 @@
 package com.livo.library_service.shared.globalExceptions;
 
-public abstract class ApplicationException extends RuntimeException {
-    private final String field;
+import org.springframework.http.HttpStatus;
 
-    protected ApplicationException(String field, String message) {
+public abstract class ApplicationException extends RuntimeException {
+
+    private final String field;
+    private final HttpStatus status;
+
+    protected ApplicationException(String field, String message, HttpStatus status) {
         super(message);
         this.field = field;
+        this.status = status;
     }
 
     public String getField() {
         return field;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 }
