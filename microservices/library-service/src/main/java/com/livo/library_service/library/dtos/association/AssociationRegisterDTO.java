ab38fb8e77dd97cfe6b2a04781968a0decd6580c
kauan <kauancavalcante8p@gmail.com>
2025-11-19 17:44:50 -0300

    add: melhorando as validações do controler

--- microservices/library-service/src/main/java/com/livo/library_service/library/dtos/association/AssociationRegisterDTO.java
+++ microservices/library-service/src/main/java/com/livo/library_service/library/dtos/association/AssociationRegisterDTO.java
@@ -1,6 +1,9 @@
 package com.livo.library_service.library.dtos.association;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,14 +14,22 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AssociationRegisterDTO {
+
     @NotNull(message = "UserId é obrigatório")
     private UUID userId;
 
-    @NotNull(message = "BookId é obrigatório")
+    @NotBlank(message = "BookId não pode estar vazio")
+    @Size(max = 50, message = "BookId não pode ter mais que 50 caracteres")
     private String bookId;
 
+    @Pattern(
+            regexp = "^(https?|ftp)://[^\\s/$.?#].[^\\s]*$",
+            message = "Thumbnail deve ser uma URL válida"
+    )
     private String thumbnail;
 
-    @NotNull(message = "Title é obrigatório")
+    @NotBlank(message = "Título não pode estar vazio")
+    @Size(max = 200, message = "Título não pode ter mais que 200 caracteres")
     private String title;
 }
+
