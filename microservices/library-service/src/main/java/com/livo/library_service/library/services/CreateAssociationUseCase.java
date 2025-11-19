ba98d42ac6573a13003d0c67b15ddba28021a599
kauan <kauancavalcante8p@gmail.com>
2025-11-19 17:26:43 -0300

    feat: verificando se user e book existem ao passar um associateRegisterDTO

--- microservices/library-service/src/main/java/com/livo/library_service/library/services/CreateAssociationUseCase.java
+++ microservices/library-service/src/main/java/com/livo/library_service/library/services/CreateAssociationUseCase.java
@@ -7,6 +7,9 @@ import com.livo.library_service.library.dtos.association.AssociationResponseDTO;
 import com.livo.library_service.library.mappers.AssociationMappers;
 import com.livo.library_service.shared.clients.BookClient;
 import com.livo.library_service.shared.clients.UserClient;
+import com.livo.library_service.shared.globalExceptions.custon.BookNotFoundException;
+import com.livo.library_service.shared.globalExceptions.custon.UserNotFoundException;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +24,17 @@ public class CreateAssociationUseCase {
 
     public AssociationResponseDTO execute(AssociationRegisterDTO register) {
 
+        try {
+            userClient.getById(register.getUserId());
+        } catch (FeignException.NotFound ex) {
+            throw new UserNotFoundException(register.getUserId());
+        }
 
+        try {
+            bookClient.getBookById(register.getBookId());
+        } catch (FeignException.NotFound ex) {
+            throw new BookNotFoundException(register.getBookId());
+        }
 
         UserBookEntity entity = associationMappers.toEntity(register);
         UserBookEntity saved = libraryRepository.save(entity);
