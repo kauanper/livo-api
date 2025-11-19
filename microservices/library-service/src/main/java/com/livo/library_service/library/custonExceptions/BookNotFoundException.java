2be6eef8c12299abc48c208e2f131a04bacd13c4
kauan <kauancavalcante8p@gmail.com>
2025-11-19 14:40:56 -0300

    add: BookNotFoundException

---
+++ microservices/library-service/src/main/java/com/livo/library_service/library/custonExceptions/BookNotFoundException.java
@@ -0,0 +1,12 @@
+package com.livo.library_service.library.custonExceptions;
+
+import com.livo.library_service.shared.globalExceptions.ApplicationException;
+
+public class BookNotFoundException extends ApplicationException {
+    public BookNotFoundException(String bookId) {
+        super(
+                "bookId",
+                String.format("Livro com o ID '%s' não foi encontrado na biblioteca pessoal.", bookId)
+        );
+    }
+}
