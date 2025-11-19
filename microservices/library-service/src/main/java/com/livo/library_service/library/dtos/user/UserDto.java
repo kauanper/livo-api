9108b90c891c8256a2e4051f40a1c6b0fef9be76
kauan <kauancavalcante8p@gmail.com>
2025-11-19 16:02:28 -0300

    add: validando a chamanda interna com user-service usando o OpenFeign

--- microservices/library-service/src/main/java/com/livo/library_service/library/dtos/user/UserDto.java
+++ microservices/library-service/src/main/java/com/livo/library_service/library/dtos/user/UserDto.java
@@ -1,4 +1,4 @@
-package com.livo.library_service.library.dtos;
+package com.livo.library_service.library.dtos.user;
 
 import java.util.UUID;
 
--- microservices/library-service/src/main/java/com/livo/library_service/shared/clients/UserClient.java
+++ microservices/library-service/src/main/java/com/livo/library_service/shared/clients/UserClient.java
@@ -1,15 +1,18 @@
 package com.livo.library_service.shared.clients;
 
-import com.livo.library_service.library.dtos.UserDto;
+import com.livo.library_service.library.dtos.user.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
-@FeignClient(name = "user-service")
+@FeignClient(
+        name = "user-service",
+        url = "http://localhost:8082"
+)
 public interface UserClient {
-
     @GetMapping("/user/internal/users/{id}")
     UserDto getById(@PathVariable("id") UUID id);
 }
+
