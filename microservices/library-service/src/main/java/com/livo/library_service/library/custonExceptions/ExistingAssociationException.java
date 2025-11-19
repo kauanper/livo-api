66b69681ee36c4b609c2a46b21982f5e4ff9df91
kauan <kauancavalcante8p@gmail.com>
2025-11-19 17:32:49 -0300

    add: ExistingAssociationException

---
+++ microservices/library-service/src/main/java/com/livo/library_service/library/custonExceptions/ExistingAssociationException.java
@@ -0,0 +1,14 @@
+package com.livo.library_service.library.custonExceptions;
+
+import com.livo.library_service.shared.globalExceptions.ApplicationException;
+import org.springframework.http.HttpStatus;
+
+public class ExistingAssociationException extends ApplicationException {
+    public ExistingAssociationException(String message) {
+        super(
+                "UserId e BookId",
+                String.format("Usuário já possui esse livro em sua biblioteca pessoal"),
+                HttpStatus.CONFLICT
+        );
+    }
+}
