46643671ff2a69b1311102eeebae2cf852509f23
kauan <kauancavalcante8p@gmail.com>
2025-11-18 21:54:12 -0300

    add: implementando a logica ListLibraryBooksUseCase

--- microservices/library-service/src/main/java/com/livo/library_service/library/LibraryRepository.java
+++ microservices/library-service/src/main/java/com/livo/library_service/library/LibraryRepository.java
@@ -2,5 +2,9 @@ package com.livo.library_service.library;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface LibraryRepository extends JpaRepository<UserBookEntity, Long> {
+    List<UserBookEntity> findAllByUserId(UUID userId);
 }
--- microservices/library-service/src/main/java/com/livo/library_service/library/services/ListLibraryBooksUseCase.java
+++ microservices/library-service/src/main/java/com/livo/library_service/library/services/ListLibraryBooksUseCase.java
@@ -1,20 +1,28 @@
 package com.livo.library_service.library.services;
 
 import com.livo.library_service.library.LibraryRepository;
+import com.livo.library_service.library.UserBookEntity;
 import com.livo.library_service.library.dtos.AssociationResponseDTO;
+import com.livo.library_service.library.mappers.AssociationMappers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ListLibraryBooksUseCase {
 
     private final LibraryRepository libraryRepository;
+    private final AssociationMappers associationMappers;
 
     public List<AssociationResponseDTO> execute(UUID userId) {
+        List<UserBookEntity> entities = libraryRepository.findAllByUserId(userId);
 
+        return entities.stream()
+                .map(AssociationMappers::toResponseDTO)
+                .collect(Collectors.toList());
     }
 }
