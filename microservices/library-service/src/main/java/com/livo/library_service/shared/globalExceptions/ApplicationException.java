38cd9a53841d4c1f8465c163e456490650e521ec
kauan <kauancavalcante8p@gmail.com>
2025-11-19 10:52:03 -0300

    add: aplicationException

---
+++ microservices/library-service/src/main/java/com/livo/library_service/shared/globalExceptions/ApplicationException.java
@@ -0,0 +1,10 @@
+package com.livo.library_service.shared.globalExceptions;
+
+public abstract class ApplicationException extends RuntimeException {
+    private final String field;
+
+    protected ApplicationException(String field, String message) {
+        super(message);
+        this.field = field;
+    }
+}
