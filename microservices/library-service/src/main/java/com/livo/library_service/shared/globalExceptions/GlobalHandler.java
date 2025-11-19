7a6d29cf050f536a899a773a7e9acb49c0648116
kauan <kauancavalcante8p@gmail.com>
2025-11-19 14:42:00 -0300

    add: BookNotFoundException em globalHandler

--- microservices/library-service/src/main/java/com/livo/library_service/shared/globalExceptions/GlobalHandler.java
+++ microservices/library-service/src/main/java/com/livo/library_service/shared/globalExceptions/GlobalHandler.java
@@ -1,5 +1,6 @@
 package com.livo.library_service.shared.globalExceptions;
 
+import com.livo.library_service.library.custonExceptions.BookNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -23,4 +24,17 @@ public class GlobalHandler {
         );
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleBookNotFound(BookNotFoundException ex) {
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                ex.getMessage(),
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                "Book Not Found"
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
 }
