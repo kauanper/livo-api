d9678650aa0f7b3f87cb02b358b474e3b8c5aa1b
Miqueias Bento <miqueias.bento.da.silva@gmail.com>
2025-11-29 18:11:16 -0300

    fix(library-service): tipo da lista de IDs de entrada para POST de Shelfs

--- microservices/library-service/src/main/java/com/livo/library_service/shelf/entity/dtos/ShelfPostDto.java
+++ microservices/library-service/src/main/java/com/livo/library_service/shelf/entity/dtos/ShelfPostDto.java
@@ -1,11 +1,10 @@
 package com.livo.library_service.shelf.entity.dtos;
 
 import java.util.List;
-import java.util.UUID;
 
 public record ShelfPostDto(
         String name,
         String description,
-        List<UUID> books
+        List<Long> books
 ) {
 }
