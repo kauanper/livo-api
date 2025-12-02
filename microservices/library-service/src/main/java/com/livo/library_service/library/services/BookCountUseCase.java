10e2ed66cb74133b50ad6ef08b46bbbd770b0663
kauan <kauancavalcante8p@gmail.com>
2025-12-01 23:47:53 -0300

    add: controller e service para contagem de livros por status

---
+++ microservices/library-service/src/main/java/com/livo/library_service/library/services/BookCountUseCase.java
@@ -0,0 +1,10 @@
+package com.livo.library_service.library.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookCountUseCase {
+
+}
