c3acf55f81bc53412c4f5090a28dc6fd6b5a8e1d
kauan <kauancavalcante8p@gmail.com>
2025-12-01 22:16:48 -0300

    add: campo de reading e read

--- microservices/user-service/src/main/java/com/livo/user_service/user/dto/UserProfileResponse.java
+++ microservices/user-service/src/main/java/com/livo/user_service/user/dto/UserProfileResponse.java
@@ -3,6 +3,8 @@ package com.livo.user_service.user.dto;
 public record UserProfileResponse(
         String username,
         String email,
-        String profilePictureUrl
+        String profilePictureUrl,
+        int reading,
+        int read
 ) {
 }
