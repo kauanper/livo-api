8dd5e3dac08348b9a946108f85ae886172b7c19e
kauan <kauancavalcante8p@gmail.com>
2025-12-02 00:26:40 -0300

    add: algortimo para pegar quatidade de livro por status

--- microservices/library-service/src/main/java/com/livo/library_service/library/services/BookCountUseCase.java
+++ microservices/library-service/src/main/java/com/livo/library_service/library/services/BookCountUseCase.java
@@ -1,10 +1,46 @@
 package com.livo.library_service.library.services;
 
+import com.livo.library_service.library.BookStatus;
+import com.livo.library_service.library.LibraryRepository;
+import com.livo.library_service.library.UserBookEntity;
+import com.livo.library_service.library.dtos.book_count.BookCountResponse;
+import com.livo.library_service.shared.clients.UserClient;
+import com.livo.library_service.shared.globalExceptions.custon.UserNotFoundException;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class BookCountUseCase {
 
+    private final LibraryRepository libraryRepository;
+
+    public BookCountResponse execute(UUID userId) {
+
+
+        List<UserBookEntity> books;
+        int[] counts = new int[BookStatus.values().length + 1];
+
+
+        for(int i = 0; i < BookStatus.values().length; i++) {
+            BookStatus status = BookStatus.values()[i];
+
+            books = libraryRepository.findAllByUserIdAndBookStatus(userId, status);
+            counts[i] = books.size();
+            counts[BookStatus.values().length] += counts[i];
+        }
+
+        return new BookCountResponse(
+                counts[0], //QUERO_LER
+                counts[1], //LENDO
+                counts[2], //LIDO
+                counts[3], //ABANDONADO
+                counts[4]  //TOTAL
+        );
+    }
 }
