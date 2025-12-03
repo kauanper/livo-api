8be776ce16793d63d14e6af4fb76f6a946f61253
kauan <kauancavalcante8p@gmail.com>
2025-12-02 22:44:20 -0300

    add: dto para resposta de livros do user

---
+++ microservices/book-service/src/main/java/com/livo/book_service/APIs/LibraryClient.java
@@ -0,0 +1,4 @@
+package com.livo.book_service.APIs;
+
+public class LibraryClient {
+}
---
+++ microservices/library-service/src/main/java/com/livo/library_service/library/dtos/book_count/ListBooksIdResponse.java
@@ -0,0 +1,8 @@
+package com.livo.library_service.library.dtos.book_count;
+
+import java.util.List;
+
+public record ListBooksIdResponse (
+        List<String> booksId
+){
+}
