31c852d4576804c4f0744de566eda61f56000dd1
kauan <kauancavalcante8p@gmail.com>
2025-12-03 00:46:16 -0300

    error: open feign de library deu problema pq agora exigi um usuario autenticado

--- microservices/book-service/src/main/java/com/livo/book_service/services/GetBookByIdUseCase.java
+++ microservices/book-service/src/main/java/com/livo/book_service/services/GetBookByIdUseCase.java
@@ -1,6 +1,8 @@
 package com.livo.book_service.services;
 
 import com.livo.book_service.APIs.GoogleBooksClient;
+import com.livo.book_service.APIs.LibraryClient;
+import com.livo.book_service.dtos.BookIdResponse;
 import com.livo.book_service.dtos.BookSummaryResponse;
 import com.livo.book_service.exceptions.custom.BookNotFoundException;
 import com.livo.book_service.exceptions.custom.InvalidRequestException;
@@ -9,15 +11,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class GetBookByIdUseCase {
 
     private final GoogleBooksClient googleBooksClient;
     private final BookMapper bookMapper;
+    private final LibraryClient libraryClient;
 
     @Cacheable(value = "bookById", key = "#bookId")
-    public BookSummaryResponse execute(String bookId) {
+    public BookSummaryResponse execute(String bookId, UUID userId) {
 
         if (bookId == null || bookId.isBlank()) {
             throw new InvalidRequestException("O ID do livro não pode estar vazio.");
@@ -28,6 +36,21 @@ public class GetBookByIdUseCase {
         if (bookItem == null || bookItem.getVolumeInfo() == null) {
             throw new BookNotFoundException("Livro com ID '" + bookId + "' não foi encontrado.");
         }
+
+        //buscar IDs da biblioteca do usuário
+        List<BookIdResponse> booksIdResponse = libraryClient.getBooksId(userId);
+
+        //transformar em Set para busca eficiente
+        Set<String> userBookIds = booksIdResponse.stream()
+                .map(BookIdResponse::getBookId)
+                .collect(Collectors.toSet());
+
+        //mapear GoogleBook → BookSummaryResponse
+        BookSummaryResponse response = bookMapper.toSummary(bookItem);
+
+        //verificar se o usuário possui esse livro
+        boolean hasBook = userBookIds.contains(bookId);
+        response.setPersonalLibrary(hasBook);
 
         return bookMapper.toSummary(bookItem);
     }
