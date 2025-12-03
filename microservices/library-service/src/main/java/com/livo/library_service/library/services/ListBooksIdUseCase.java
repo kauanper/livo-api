af36c054804c9a94c667018106bc8885848eea80
kauan <kauancavalcante8p@gmail.com>
2025-12-02 23:19:15 -0300

    add: controller e service para para retornar a lista com os is dos livros

--- microservices/library-service/src/main/java/com/livo/library_service/library/services/ListBooksIdUseCase.java
+++ microservices/library-service/src/main/java/com/livo/library_service/library/services/ListBooksIdUseCase.java
@@ -1,13 +1,10 @@
 package com.livo.library_service.library.services;
 
 import com.livo.library_service.library.LibraryRepository;
-import com.livo.library_service.library.UserBookEntity;
 import com.livo.library_service.library.dtos.book_count.BookIdResponse;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.jni.Library;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
