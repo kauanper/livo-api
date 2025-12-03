0cab4586530c1e11908f4db540211113002b8d03
kauan <kauancavalcante8p@gmail.com>
2025-12-02 23:50:19 -0300

    testando currentUser

--- microservices/book-service/src/main/java/com/livo/book_service/controllers/BookController.java
+++ microservices/book-service/src/main/java/com/livo/book_service/controllers/BookController.java
@@ -3,11 +3,13 @@ package com.livo.book_service.controllers;
 import com.livo.book_service.dtos.BookSummaryResponse;
 import com.livo.book_service.services.GetBookByIdUseCase;
 import com.livo.book_service.services.search.SearchBooksUseCase;
+import com.livo.book_service.util.notations.CurrentUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/books")
@@ -21,9 +23,15 @@ public class BookController {
 
     @GetMapping("/search")
     public ResponseEntity<List<BookSummaryResponse>> searchBooks(
+            @CurrentUser UUID currentUser,
+            @RequestHeader("Authorization") String authorizationHeader,
             @RequestParam String query,
             @RequestParam String type,
             @RequestParam String orderBy) {
+        String token = authorizationHeader.replace("Bearer ", "");
+
+
+        System.out.println(" meu id é: " + currentUser);
 
         return ResponseEntity.ok(searchBooksUseCase.execute(query, type, orderBy));
     }
