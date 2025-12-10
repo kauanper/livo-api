81e0a5024f15851b247baf27ce42b80581a37c3b
kauan <kauancavalcante8p@gmail.com>
2025-12-10 18:34:01 -0300

    add: novos atributos ao dto

--- microservices/library-service/src/main/java/com/livo/library_service/library/dtos/book_count/BookIdResponse.java
+++ microservices/library-service/src/main/java/com/livo/library_service/library/dtos/book_count/BookIdResponse.java
@@ -1,6 +1,12 @@
 package com.livo.library_service.library.dtos.book_count;
 
+import com.livo.library_service.library.BookStatus;
+
 public record BookIdResponse(
-        String bookId
+        String bookId,
+        BookStatus bookStatus,
+        Integer readingProgress,
+        Integer personalRatting,
+        Integer generalRatting // no prototipo está como inteiro :b
 ){
 }
