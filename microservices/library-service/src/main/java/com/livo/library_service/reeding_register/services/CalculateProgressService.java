a2e1e04052c9ab5ede9dd6fd64a00375ecf487ea
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-12-10 18:26:51 -0300

    feat: adicionando pesquisa jpql para maior ReadingLog por pagina

--- microservices/library-service/src/main/java/com/livo/library_service/reeding_register/services/CalculateProgressService.java
+++ microservices/library-service/src/main/java/com/livo/library_service/reeding_register/services/CalculateProgressService.java
@@ -1,6 +1,7 @@
 package com.livo.library_service.reeding_register.services;
 
 import com.livo.library_service.library.services.FindBookByIdUseCase;
+import com.livo.library_service.reeding_register.ReadingLog;
 import com.livo.library_service.reeding_register.ReadingLogRepository;
 import com.livo.library_service.shared.dtos.book.BookSummaryResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Optional;
 
 @Service
 public class CalculateProgressService {
@@ -19,6 +21,16 @@ public class CalculateProgressService {
 
     public BigDecimal getReedingProgresByPagesRead(Integer pagesRead, String bookId){
         BookSummaryResponse book = findBookByIdUseCase.execute(bookId);
+        BigDecimal percentage = BigDecimal.ZERO;
+        if (book.pageCount() != null && book.pageCount() > 0 && pagesRead != null) {
+            percentage = BigDecimal.valueOf((double) pagesRead / book.pageCount() * 100)
+                    .setScale(0, RoundingMode.HALF_DOWN);
+        }
+        return percentage;
+    }
+
+    public BigDecimal getReedingProgresByLibraryBookId(Long userBookId){
+        Optional<ReadingLog> log = readingLogRepository.findMaxByLibraryBookId(userBookId);
         BigDecimal percentage = BigDecimal.ZERO;
         if (book.pageCount() != null && book.pageCount() > 0 && pagesRead != null) {
             percentage = BigDecimal.valueOf((double) pagesRead / book.pageCount() * 100)
