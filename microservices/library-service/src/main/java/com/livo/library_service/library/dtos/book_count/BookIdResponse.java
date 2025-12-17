7a741d0adc0bd6f49089835513b69efce37d448b
kauan <kauancavalcante8p@gmail.com>
2025-12-17 20:04:18 -0300

    add: update dto BookIdRes

--- microservices/library-service/src/main/java/com/livo/library_service/library/dtos/book_count/BookIdResponse.java
+++ microservices/library-service/src/main/java/com/livo/library_service/library/dtos/book_count/BookIdResponse.java
@@ -1,6 +1,15 @@
 package com.livo.library_service.library.dtos.book_count;
 
+import com.livo.library_service.library.BookStatus;
+
+import java.math.BigDecimal;
+
 public record BookIdResponse(
-        String bookId
+        String bookId,
+        Long userBookId,
+        BookStatus bookStatus,
+        BigDecimal progress,
+        Integer ratingPersonal,
+        Double averageRating
 ){
 }
