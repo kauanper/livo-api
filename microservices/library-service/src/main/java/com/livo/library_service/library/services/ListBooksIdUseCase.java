965991cac47f427ac080dd49c3b739094a355831
kauan <kauancavalcante8p@gmail.com>
2025-12-17 20:13:10 -0300

    add: chamando metodo para calcular progresso

--- microservices/library-service/src/main/java/com/livo/library_service/library/services/ListBooksIdUseCase.java
+++ microservices/library-service/src/main/java/com/livo/library_service/library/services/ListBooksIdUseCase.java
@@ -2,6 +2,7 @@ package com.livo.library_service.library.services;
 
 import com.livo.library_service.library.LibraryRepository;
 import com.livo.library_service.library.dtos.book_count.BookIdResponse;
+import com.livo.library_service.reeding_register.services.CalculateProgressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +14,23 @@ import java.util.UUID;
 public class ListBooksIdUseCase {
 
     private final LibraryRepository libraryRepository;
+    private final CalculateProgressService readingProgressService;
 
-    public List<BookIdResponse> execute(UUID userId){
+    public List<BookIdResponse> execute(UUID userId) {
         return libraryRepository.findAllByUserId(userId)
                 .stream()
-                .map(entity -> new BookIdResponse(entity.getBookId().toString()))
+                .map(entity -> new BookIdResponse(
+                        entity.getBookId().toString(),
+                        entity.getId(),
+                        entity.getBookStatus(),
+                        readingProgressService.getReadingProgressByLibraryBookId(
+                                entity.getId(),
+                                entity.getBookId().toString(),
+                                userId
+                        ),
+                        null, // rating pessoal ainda não implementado
+                        null  // média ainda não implementada
+                ))
                 .toList();
     }
 }
