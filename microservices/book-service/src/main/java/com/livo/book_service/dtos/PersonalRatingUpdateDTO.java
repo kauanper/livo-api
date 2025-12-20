b6bc95e02a530152ce3b176174a4554379865dd5
Miqueias Bento <miqueias.bento.da.silva@gmail.com>
2025-12-20 10:05:49 -0300

    feat(book-service): DTO PersonalRatingUpdateDTO para atualizar avaliacao pessoal

---
+++ microservices/book-service/src/main/java/com/livo/book_service/dtos/PersonalRatingUpdateDTO.java
@@ -0,0 +1,14 @@
+package com.livo.book_service.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonalRatingUpdateDTO {
+    private Integer personalRating;
+}
+
+
